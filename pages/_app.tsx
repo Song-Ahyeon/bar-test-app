1a912ea46acfa2f27afa5461c363c59eb9748b81
Song Ahyeon <song@5works.co.kr>
2023-04-17 13:54:21 +0900

    공통 Layout 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,17 @@ import './index.css'
 import type { AppProps } from 'next/app'
 import { Layout } from 'antd'
 import LoginModal from '../components/loginModal'
-import Header from 'antd/es/layout/layout'
-
-    //   <Header style={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-    //     <div className="logo" />
-    //     <LoginModal />
-    //   </Header>
 
 export default function App({ Component, pageProps }: AppProps) {
+const { Header, Content, Footer } = Layout;
   return(
     <Layout>
+      <Header style={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <div className="logo" />
+        <LoginModal />
+      </Header>
       <Component {...pageProps} />
-      {/* <Footer style={{ textAlign: 'center', cursor: 'pointer' }}>이용약관 | 개인정보 처리방침 | B.A.R ©2023</Footer> */}
+      <Footer style={{ textAlign: 'center', cursor: 'pointer' }}>이용약관 | 개인정보 처리방침 | B.A.R ©2023</Footer>
     </Layout>
   )
 }
