506200fc402920ff33b9c478ce0252a286514fff
Song Ahyeon <song@5works.co.kr>
2023-04-17 18:13:14 +0900

    logo 클릭시 메인페이지 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import './index.css'
 import type { AppProps } from 'next/app'
 import { Layout } from 'antd'
 import LoginModal from '../components/loginModal'
+import router from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-const { Header, Content, Footer } = Layout;
+  const { Header, Content, Footer } = Layout;
+  
+  const gotoMain = () => {
+    router.push({
+        pathname: '/',
+    })
+  }
+  
   return(
     <Layout>
       <Header style={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <div className="logo" />
+        <div className="logo" onClick={gotoMain} style={{cursor: 'pointer'}}/>
         <LoginModal />
       </Header>
       <Component {...pageProps} />
