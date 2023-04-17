e731709c67e211744e175fb38421e376f7d34e8c
Song Ahyeon <song@5works.co.kr>
2023-04-17 16:24:47 +0900

    Update index.tsx

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,7 +1,40 @@
+import SearchInput from '@/components/searchInput';
+import SearchResultList from '@/components/searchResultList';
+import { Button, Card, Layout, Pagination, theme } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    padding: 30px 5px;
+`
+
+const {Content} = Layout;
+
 const SearchPage = () => {
+    const {
+        token: { colorBgContainer },
+      } = theme.useToken();
+
+      const searchResult = [
+        {title: 'title1', author: 'author1', info: 'infotext1'},
+        {title: 'title2', author: 'author2', info: 'infotext2'},
+        {title: 'title3', author: 'author3', info: 'infotext3'},
+      ]
     return (
         <>
-        <p>search page</p>
+        <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content" style={{ background: colorBgContainer }}>
+            <SearchInput />
+            <div style={{margin: '5px 0', display: 'flex'}}>
+                <Button style={{marginLeft: 'auto'}}>등록</Button>
+            </div>
+            <Wrapper>
+                {searchResult.map((el: any, idx: number) => (
+                    <SearchResultList key={`search_result_${idx}`} props={el} />
+                ))}
+            </Wrapper>
+            <Pagination />
+        </div>
+      </Content>
         </>
     )
 }
